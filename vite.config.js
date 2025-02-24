87113b7c6b107f69a3c0ba55f5f3ecc395b380db
programmingmakito <programmingmakito@gmail.com>
2025-02-24 22:32:27 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    preview: {
-      host: true,
-      allowedHosts: ['ystime-006ddb86768b.herokuapp.com', 'localhost'],
-    },
-  },
-});
+})
